0310b9d3bf5203e84cbfae2fd4801100158c2114
thesaladbowl <nocturne486@gmail.com>
2020-05-11 16:42:01 +0900

    heroku-redis setings

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-wildf22l-ssadsadn111141322f1dwqdqdsd4e22ver-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REDIS_HOST = 'localhost'
+    REDIS_HOST = os.environ.get('REDIS_URL') or 'localhost'
     REDIS_PORT = 6379
     REDIS_DB = 0
 
